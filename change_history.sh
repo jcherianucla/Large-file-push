e3fb9b321f5973d52e7796b35f7517d1ce2f196b
Jahan Kuruvilla Cherian <jcherian@ucla.edu>
2016-07-19 16:59:05 -0700

    Add files via upload
    Needs fixes for customization

--- change_history.sh
+++ change_history.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 
+DIR="/cygdrive/d/Sandbox/"
+TIME=1
+CHECKOUT_DIR="./checkout/version3.1"
+
+if [[ $1 == "d" || $1 == "dir" ]]; then
+	echo "Enter a valid project directory to search through"
+	read DIR
+	echo "Enter a valid time [1 for past day, 2 for past 2 days etc] to search through"
+	read TIME
+fi
+
 echo "Searching..."
 
-changed_files=($(find /cygdrive/d/Sandbox/ -type f -mtime -1 -regextype "posix-extended" -iregex '.*\.(h|cpp)')) 
+changed_files=($(find $DIR -type f -mtime -$TIME -regextype "posix-extended" -iregex '.*\.(h|cpp)')) 
 
-if [[ $1 == "show" || $1 == "s" ]]; then
+if [[ $1 == "show" || $1 == "s"  || $2 == "show" || $2 == "s" ]]; then
 	printf '%s\n' "${changed_files[@]}"
 	echo "Done"
 	exit 1
+fi
+
+echo "Do you want to use $CHECKOUT_DIR as the checkout dir? [y/n]"
+read yn
+if [[ $yn == "no" || $yn == "n" ]]; then
+	echo "Please specify a valid git directory"
+	read CHECKOUT_DIR
 fi
 
 echo "Copying..."
 
 for file in ${changed_files[@]}; do
 	echo $file
-	cp $file ~/checkout/version3.1
+	cp $file $CHECKOUT_DIR
 done
 
-echo "State of checkout/version3.1"
-ls ./checkout/version3.1/
+echo "State of $CHECKOUT_DIR"
+ls $CHECKOUT_DIR
+
 echo "Do you want to check files in? [y/n] "
 
 read yesno
